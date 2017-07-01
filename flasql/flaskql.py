fdf01a6b4c209b05c1c85ad31d8e3936d0e6cd7c
mrmiguez <r.m.miguez@gmail.com>
2017-07-01 15:10:00 -0400

    dynamic page generation from local dc & oai

--- flasql/flaskql.py
+++ flasql/flaskql.py
@@ -1,14 +1,14 @@
 import os
 import sqlite3
 from sickle import Sickle
-from pymods import MODSReader
+from pymods import MODSReader, OAIReader
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
 app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'MODSdebugSmall.xml'),
+    DATABASE=os.path.join(app.root_path, 'DCdebugSmall.xml'),
     SECRET_KEY='potato',
     USERNAME='admin',
     PASSWORD='default',
@@ -17,48 +17,38 @@ app.config.update(dict(
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 
-def parse_MODS():
-    records = MODSReader(app.config['DATABASE'])
+def parse_local_dc():
+    records = OAIReader(app.config['DATABASE'])
     return records
+
 
 def get_oai():
     oai_feed = Sickle(app.config['OAI'])
     records = oai_feed.ListRecords(metadataPrefix='oai_dc', set='scbooks', ignore_deleted=True)
     return records
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')  # , elems=os.listdir('templates/'))
+    return render_template('index.html')
 
-@app.route('/titles')
-def titles():
-    title_list = []
-    for record in parse_MODS():
-        for title in record.titles:
-            title_list.append(title)
-    return render_template('titles.html', titles=title_list)
 
-@app.route('/creators')
-def elem_page():
-    elem_list = [name.text for record in parse_MODS() for name in record.get_creators]
+@app.route('/<elem>')
+def local_elem_page(elem):
+    dc_ns = '{http://purl.org/dc/elements/1.1/}'
+    records = parse_local_dc()
+    try:
+        elem_list = [value for record in records for value in record.metadata.get_element(dc_ns + elem)]
+    except TypeError:
+        pass
     return render_template('titles.html', titles=elem_list)
 
-# @app.route('/oai/<elem>')
-# def oai_elem_page(elem):
-#     try:
-#         elem_list = [value for record in get_oai() for value in record.metadata['{}'.format(elem)]]
-#         return render_template('titles.html', titles=elem_list)
-#     except KeyError:
-#         pass
 
-
-@app.route('/oai/creator')
-def oai_elem_page():
+@app.route('/oai/<elem>')
+def oai_elem_page(elem):
     records = get_oai()
-    elem_list = []
     try:
-        elem_list = [value for record in records for value in record.metadata['creator']]
-        print(elem_list)
-    except KeyError:
+        elem_list = [value for record in records for value in record.metadata[elem]]
+    except TypeError:
         pass
     return render_template('titles.html', titles=elem_list)
